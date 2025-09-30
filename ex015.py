492e65160fe8707bc265d3355cddef48fd3116cd
moisegit528 <moisesgithub@gmail.com>
2025-09-30 14:20:39 -0300

    correção de português

--- ex015.py
+++ ex015.py
@@ -1,4 +1,4 @@
 dias = int(input("Quantos dias alugados? "))
-km = float(input("Quanbtos Km rodados? "))
+km = float(input("Quantos Km rodados? "))
 soma = (dias * 60) + (km * 0.15) # por km rodados
 print("O total a pagar é de R${:.2f}".format(soma))
